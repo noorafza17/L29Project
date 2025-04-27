2ef373b6769be178e61970c48d6521a79558ae41
noorafza17 <noorafza6851@gmail.com>
2025-04-27 11:56:23 +0530

    Add files via upload

--- L29Project.py
+++ L29Project.py
@@ -14,7 +14,10 @@ class Bus(Automobiles):
         self.company=company
     def details(self):
         print(self.wheeler,self.windows,self.colour,self.company)
+    def busfair(self,no_of_passenger):
+        print("the bus fair is",50*no_of_passenger)
 SchoolBus= Bus(4,True,"yellow","ABC")
 SchoolBus.details()
+SchoolBus.busfair(10)
 
 #how to calculate the fair??
